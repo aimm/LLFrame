6bfca7cd7db63176aa946736f0ab2b2839d6bf9f
Hailong Guo <hailongguo@hailongdembp.lan>
2017-12-19 08:46:54 +0800

    Initial Commit

---
+++ LLFrame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLFrame
+//
+//  Created by Hailong Guo on 2017/12/19.
+//  Copyright © 2017年 Hailong Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
